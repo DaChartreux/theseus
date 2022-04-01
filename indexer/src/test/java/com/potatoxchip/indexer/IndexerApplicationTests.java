ac67c8ed8df514ec11c2332414dad4fc0d55df0c
Potato Chip <thepotatoxchip@gmail.com>
2022-04-01 17:54:12 +0530

    :sparkles: chore: add indexer service

---
+++ indexer/src/test/java/com/potatoxchip/indexer/IndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatoxchip.indexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
